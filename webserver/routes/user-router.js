e752e10c152431214725925e891c581a0ce4af35
Edu Perez <eduper11@gmail.com>
2019-05-29 12:10:39 +0200

    modify endpoint

--- webserver/routes/user-router.js
+++ webserver/routes/user-router.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
-const express = require("express");
-const multer = require("multer");
-const checkJwt = require("../controllers/session/check-jwt-token");
-const getUserProfile = require("../controllers/user/get-user-profile-controller");
-const updateProfile = require("../controllers/user/update-user-profile-controller");
-const uploadAvatar = require("../controllers/user/upload-avatar-controller");
-const searchUser = require("../controllers/user/search-users-controller");
+const express = require('express');
+const multer = require('multer');
+const checkJwt = require('../controllers/session/check-jwt-token');
+const getUserProfile = require('../controllers/user/get-user-profile-controller');
+const updateProfile = require('../controllers/user/update-user-profile-controller');
+const uploadAvatar = require('../controllers/user/upload-avatar-controller');
+const searchUser = require('../controllers/user/search-users-controller');
 
 const upload = multer();
 const router = express.Router();
 
-router.get("/user/profile", checkJwt, getUserProfile);
-router.put("/user/profile", checkJwt, updateProfile);
-router.post("/user/avatar", checkJwt, upload.single("avatar"), uploadAvatar);
-router.get("/user/search", checkJwt, searchUser);
+router.get('/user', checkJwt, getUserProfile);
+router.put('/user', checkJwt, updateProfile);
+router.post('/user/avatar', checkJwt, upload.single('avatar'), uploadAvatar);
+router.get('/user/search', checkJwt, searchUser);
 
 module.exports = router;
