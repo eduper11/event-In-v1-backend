035d4c7386a92d2670393bc6a69d8ecf86defa9e
Edu Perez <eduper11@gmail.com>
2019-06-04 16:58:41 +0200

    getEventByUuid

--- webserver/controllers/event/get-event-by-uuid-controller.js
+++ webserver/controllers/event/get-event-by-uuid-controller.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
-const mysqlPool = require("../../../databases/mysql-pool");
+const mysqlPool = require('../../../databases/mysql-pool');
 
 async function getEventList(req, res) {
   const connection = await mysqlPool.getConnection();
   const { uuid } = req.claims;
-  const sqlQuery = `SELECT events.event_id, name, owner_uuid, events.company, events.created_at, finish_at, finished FROM events
+  const sqlQuery = `SELECT events.id, name, owner_uuid, events.company, events.created_at, finish_at, finished FROM events
 INNER JOIN user_events ON events.id = user_events.event_id 
 INNER JOIN users ON user_events.user_id = users.id 
 WHERE uuid = '${uuid}';`;
