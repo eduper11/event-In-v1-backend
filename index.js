fb863bd40121e1e8d8a09aad4bd5b56705059b49
Edu Perez <eduper11@gmail.com>
2019-06-06 23:32:48 +0200

    test perso err

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use((err, req, res, next) => {
 
   if (name !== 'Error') {
     const { message, status } = err;
-
+    console.error(err);
     return res.status(status).send({
       name,
       message,
