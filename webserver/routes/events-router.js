2f4e6fa4001cd79fd237f4d87546ef9e3829ab4e
Edu Perez <eduper11@gmail.com>
2019-06-10 16:39:45 +0200

    event/s endpoint modification

--- webserver/routes/events-router.js
+++ webserver/routes/events-router.js
@@ -11,7 +11,7 @@ const getEventJoined = require('../controllers/event/get-event-joined-controller
 const getEventById = require('../controllers/event/get-event-by-id-controller');
 const router = express.Router();
 
-router.get('/event', checkJwt, getEventNotJoined);
+router.get('/events', checkJwt, getEventNotJoined);
 router.get('/myevents', checkJwt, getEventJoined);
 router.get('/event/users', checkJwt, getUserByEvent);
 router.get('/eventbyid', checkJwt, getEventById);
