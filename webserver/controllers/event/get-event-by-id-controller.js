935448f12be6ef9e2f6df6dfe202b8750b5d63c5
Edu Perez <eduper11@gmail.com>
2019-06-12 09:46:03 +0200

    query correction order get event by id

--- webserver/controllers/event/get-event-by-id-controller.js
+++ webserver/controllers/event/get-event-by-id-controller.js
@@ -6,15 +6,14 @@ async function getEventByIdList(req, res) {
   const connection = await mysqlPool.getConnection();
   const { event_id: id } = req.query;
   const sqlQuery = `SELECT id, name, owner_uuid, company, created_at, finish_at FROM events
-WHERE id = '${id}' 
-ORDER BY name`;
+WHERE id = '${id}'`;
 
   try {
-    const [eventList] = await connection.query(sqlQuery);
+    const [event] = await connection.query(sqlQuery);
 
     connection.release();
 
-    return res.status(200).send(eventList);
+    return res.status(200).send(event);
   } catch (e) {
     return res.status(500).send(e.message);
   }
